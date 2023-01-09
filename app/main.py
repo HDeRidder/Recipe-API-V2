a6d40f58ea9bc5eb185d29ef87bb9d6be66fd0a4
HaroldDeRidder <r0720137@student.thomasmore.be>
2023-01-09 21:05:39 +0100

    remove database files

--- app/main.py
+++ app/main.py
@@ -1,28 +1,8 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI
 from pydantic import BaseModel
-from sqlalchemy.orm import Session
-
-import crud
-import models
-import schemas
-from database import SessionLocal, engine
-import os
-
-if not os.path.exists('.\sqlitedb'):
-    os.makedirs('.\sqlitedb')
-
-#"sqlite:///./sqlitedb/sqlitedata.db"
-models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
 class Recipe(BaseModel):
     id: int
@@ -62,11 +42,11 @@ recipes = [
 
 #Recipe endpoints
 
-@app.get("/recipes", response_model=schemas.Recipe)
+@app.get("/recipes")
 def read_recipes():
     return recipes
 
-@app.get("/recipes/{id}", response_model=schemas.Recipe)
+@app.get("/recipes/{id}")
 def read_recipe(id: int):
     for recipe in recipes:
         if recipe.id == id:
@@ -152,25 +132,3 @@ def delete_recipe(recipe_id: int):
     recipe = next((r for r in recipes if r.id == recipe_id), None)
     if recipe is None:
         return {"error": "Recipe not found"}
-
-
-@app.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user(db=db, user=user)
-
-
-@app.get("/users/", response_model=list[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = crud.get_users(db, skip=skip, limit=limit)
-    return users
-
-
-@app.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
